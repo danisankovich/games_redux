203a4127c212cac83a2ab7b6b5aae9acd9155e66
Michael Sankovich <misankovich@gmail.com>
2016-06-26 17:21:21 -0400

    initial

---
+++ src/containers/game-list.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+class GameList extends Component {
+  renderList() {
+    return this.props.games.map((game) => {
+      return (
+        <li key={game.title} className="list-group-item">{game.title}</li>
+      )
+    });
+  }
+
+  render() {
+    return (
+      <ul className="list-group col-sm-4">
+        {this.renderList()}
+      </ul>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  // Whatever is returned will show up as props inside of gamelist
+  return {
+    games: state.games
+  };
+}
+
+export default connect(mapStateToProps)(GameList);
---
+++ src/reducers/reducer_games.js
@@ -0,0 +1,9 @@
+export default function() {
+  return [
+    {title: 'The Legend of Zelda: Ocarina of Time'},
+    {title: 'The Legend of Zelda: The Windwaker'},
+    {title: 'Banjo Kazooie'},
+    {title: 'Dragon Quest III'},
+    {title: 'Dragon Warrior Monsters'},
+  ]
+}
